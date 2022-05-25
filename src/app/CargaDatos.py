f85f3065d761968d3f2b32f66c09ae2d14e41582
Mariojj <mariojj@live.com>
2022-05-25 18:32:09 +0200

    introduciendo algunos crear modulos y empezar test

--- src/app/CargaDatos.py
+++ src/app/CargaDatos.py
@@ -1,6 +1,11 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
+
 
 class CargaCSV:
-    def __init__(self):
-        pass
+    def __init__(self,csv_path):
+        self.csv_path = csv_path
+        df=pd.read_csv(csv_path)
+        print(df.head())
+        
+
--- src/unittest/TestCargaDatos.py
+++ src/unittest/TestCargaDatos.py
@@ -1,10 +1,20 @@
-import CMI-Automatizado-Optimizado.src.app.CargaDatos as cargaDatos
+
+
+import src.app.CargaDatos as cargaDatos
 import numpy as np
 import pandas as pd
 import unittest
 
 
 class TestCargaDatos(unittest.TestCase):
-
+    
     def test_cargaCSV(self):
-        pass
+        data_path= "../../dataset/data_sample_ventas.csv"
+        archivoCSV = cargaDatos.CargaCSV(data_path)
+        self.assertEqual(1,archivoCSV.head())
+        
+
+
+
+if __name__ == '__main__':
+    unittest.main()
