3d2b174d54e0f841a4f16d6a3161516000c24c55
Mariojj <mariojj@live.com>
2022-05-26 11:52:12 +0200

    haciendo algunos cambios para poder probar test

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="paq_CMI-Automatizado-Optimizado",
+    version="1.0",
+    description= "Paquete del proyecto de CMI-Automatizado-Optimizado",
+    author= "Mario JJ",
+    author_email='mariojj@live.com',
+    url='https://www.CMI-Automatizado-Optimizado.com',
+    packages=find_packages(exclude=('doc', 'docs',)),
+    )
--- src/unittest/TestCargaDatos.py
+++ src/unittest/TestCargaDatos.py
@@ -1,6 +1,6 @@
 
 
-import src.app.CargaDatos as cargaDatos
+import CargaDatos as Load
 import numpy as np
 import pandas as pd
 import unittest
@@ -10,7 +10,7 @@ class TestCargaDatos(unittest.TestCase):
     
     def test_cargaCSV(self):
         data_path= "../../dataset/data_sample_ventas.csv"
-        archivoCSV = cargaDatos.CargaCSV(data_path)
+        archivoCSV = Load.CargaCSV(data_path)
         self.assertEqual(1,archivoCSV.head())
         
 
