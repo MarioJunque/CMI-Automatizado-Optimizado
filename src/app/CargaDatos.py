7083d75f215ccd4f37008101abf033ad1dc296c6
Mariojj <mariojj@live.com>
2022-05-29 16:42:51 +0200

    Haciendo pruebas para archivos excel

--- src/app/CargaDatos.py
+++ src/app/CargaDatos.py
@@ -4,7 +4,10 @@ import pandas as pd
 
 class CargaCSV:
     def __init__(self,csv_path):
-        self.csv_path = csv_path
-        df=pd.read_csv(self.csv_path)   
+        df=pd.read_csv(csv_path)   
         return df
 
+class CargaXSLX:
+    def __init__(self,excel_path):
+        df=pd.read_excel(excel_path)   
+        return df
--- src/unittest/TestCargaDatos.py
+++ src/unittest/TestCargaDatos.py
@@ -1,6 +1,6 @@
 
 
-import src.app.CargaDatos as Load
+from src.app.CargaDatos import CargaCSV as Load
 import numpy as np
 import pandas as pd
 import unittest
@@ -10,9 +10,13 @@ class TestCargaDatos(unittest.TestCase):
     
     def test_cargaCSV(self):
         data_path= "../../dataset/atp_matches_1997.csv"
-        archivoCSV = Load.CargaCSV(data_path)
-        print(archivoCSV)
-        
+        df = Load(data_path)
+        self.assertFalse(0,df.head(1).isEmpty)
+
+    #def test_cargaXSLX(self):
+    #    data_path= "../../dataset/superstore_sales.xlsx"
+    #    archivoXSLX = Load.CargaXSLX(data_path)
+    #    self.assertEqual(1,archivoXSLX)
 
 
 
