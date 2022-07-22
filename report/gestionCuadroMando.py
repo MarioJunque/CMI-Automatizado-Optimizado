e9d912e8b16ffd888f97bd9dc7cee23bd7d2c50b
Mariojj <mariojj@live.com>
2022-07-22 21:41:09 +0200

    cambios para usar plantilla predefinida de powerBI

--- report/gestionCuadroMando.py
+++ report/gestionCuadroMando.py
@@ -2,11 +2,5 @@ import subprocess
 
 CUADRO_DIR = "cd..\\..\\report\\plantilla.pbix"
 
-def CrearCuadroMando():
-    cadena = "C:\\Users\\mario\\AppData\\Local\\Microsoft\\WindowsApps\\Microsoft.MicrosoftPowerBIDesktop_8wekyb3d8bbwe\\PBIDesktopStore.exe"
-    #path = cadena.replace( "\",'\\')
-    subprocess.run(cadena , shell = True)
-    return True
-
 def VisualizarCuadroMando():
      subprocess.run(CUADRO_DIR , shell = True)
