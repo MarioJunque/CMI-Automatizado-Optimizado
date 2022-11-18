978bbc20749176ce0324644ef25749e670b8292e
Mariojj <mariojj@live.com>
2022-11-18 15:11:04 +0100

    proceso de optimizacion completamente funcional

--- src/app/OptimizationProcess.py
+++ src/app/OptimizationProcess.py
@@ -1,3 +1,6 @@
+
+# Carga de librerias
+
 from src.app import Training
 from src.app import DataWrangling
 from zipfile import ZipFile
@@ -6,14 +9,17 @@ import pandas as pd
 import numpy as np
 import random 
 
+# Definición de variables globales
+
 data_sales = None
+df_sales = None
 data_store = None
 data = None
 df_tam_pred = None
 informe = None
 
 
-# Comienza el proceso de optimización
+# Ejecucion el proceso de optimización
 
 def Optimizar(df):
     PrepararDatos(df)
@@ -24,13 +30,14 @@ def Optimizar(df):
 # Convierte el dataset en un dataframe para poder manejar la informacion de forma mas sencilla y limpia los datos 
 
 def PrepararDatos(dataset):     
-    global data_sales,data_store, data, df_tam_pred
+    global data_sales,data_store, data, df_tam_pred, df_sales 
     for file in dataset:
         if str(file) == 'sales.csv':
             diclist = {"product_id": object,"store_id": object,"date": object, "sales":float, "revenue": float, "stock":float, "price":float,"promo_type_1":object, "promo_bin_1":object,"promo_type_2":object, "promo_bin_2":object, "promo_discount_2":object, "promo_discount_type_2": object }                                        
-            data_sales = pd.read_csv(file, dtype= diclist)      
-            df_sales = data_sales.drop(columns=['promo_bin_1','promo_bin_2','promo_discount_2','promo_discount_type_2'])
-            df_sales_final = DataWrangling.Preprocesar(df_sales)
+            data_sales = pd.read_csv(file, dtype= diclist, sep= ",")      
+            df_sales = DataWrangling.Preprocesar(data_sales.iloc[0:30000])
+            print(df_sales.head())
+            df_sales_final = df_sales.drop(columns=['promo_bin_1','promo_bin_2','promo_discount_2','promo_discount_type_2'])
         elif str(file) == 'store_cities.csv':
             data_store = pd.read_csv(file)
     
@@ -41,37 +48,45 @@ def PrepararDatos(dataset):
 # Entrena el modelos con los datos preparados   
 
 def Entrenar():
-    global informe, data_sales,data, df_tam_pred
-    best_products = data.groupby("product_id")
-    best_products = best_products[best_products["product_id"].value_counts() >= 50]
-    sample_products = best_products.sample(n = df_tam_pred, replace=True )
+    global informe, data_sales,data, df_tam_pred, df_sales
+    # best_products = data['product_id'].value_counts()[:10]
+    sample_products = df_sales.sample(n = 30000, replace=True )   # n = df_tam_pred
 
     # Aqui se obtienen los datos de la predicción y las métricas del algoritmo escogido
-    modelo, informe = Training.TrainModelCV(best_products[['sales','stock']].values,data['revenue'].values)
+    modelo, informe = Training.TrainModelCV(data[['sales','price']].values,data['revenue'].values)
 
     # Copia de las predicciones en nuevos registros
-    tam = len(modelo)
+    
+    tam = 2
+    # tam = len(modelo)
     while tam:
-        new_row = sample_products[tam,:]       # usar metodo sample() de pandas para sacar una fila aleatoria y modificarla con nueva fecha y producto
-        fechaNueva = pd.to_datetime('2020-06-30')
-        new_row.at [0,'date'] = fechaNueva
-        new_row.at[0,'revenue'] =  modelo[tam]
+        print('modelo:',tam,'sample:',len(sample_products))
+        index = tam - 1 
+        new_row = sample_products.iloc[index]      
+        fechaNueva = pd.to_datetime('03/06/20')
+        new_row.at ['date'] = fechaNueva
+        new_row.at['revenue'] =  modelo[tam]
+        print(new_row)
 
-        data_sales = data_sales.append(new_row, ignore_index=True)
+        df_sales =pd.concat([df_sales,new_row.to_frame().T], ignore_index=True)
         tam-=1
+    
+    print(df_sales.tail())
+    
+
 
     return True
 
 # Convierte de vuelta a formato csv los archivos usados y se comprimen para reducir el tamaño de archivo
 
 def ModelConverter():
-    global data_sales,data_store
-    data_sales.to_csv("..\\dataset\\sales.csv", index=False)     # , encoding='utf-8-sig'
+    global df_sales
+    df_sales.to_csv("..\\dataset\\sales.csv", index=False)     # , encoding='utf-8-sig'
 
     # Comprime los archivos para que ocupen menos
 
-    with ZipFile('sales.zip', 'w') as myzip:
-        myzip.write('sales.csv')
+    #with ZipFile('sales.zip', 'w') as myzip:
+    #    myzip.write('..\dataset\sales.csv"')
 
 # Prepara los datos para el informe de estadísticas en la aplicación 
 
